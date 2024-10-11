63bf6094d491a731f78cd29192b365c0579d1eef
Deeja-ish <khadijaismail696@gmail.com>
2024-10-11 16:11:16 +0100

    correction

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const mysql = require('mysql2');
 dotenv.config();
 
 const db = mysql.createConnection ({
-   user : process.env.DB_USERNAME,
-    host : process.env.DB_HOST,
-    database : process.env.DB_NAME,
-    password : process.env.DB_PASSWORD,
-});
-
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+})
 
 //connecting to database
 db.connect((err) => {
@@ -22,50 +22,52 @@ db.connect((err) => {
 });
 
 //  question 1 
-app.get('/patients', (err, results) => {
+app.get('', (req, res) => {
     const patients = 'SELECT * FROM patients'
-    db.connect(patients, (req, res) => {
+    db.query(patients, (err, results) => {
         if (err){
             return res.status(500).send('Failed to get patients information', err)      
           }
           res.status(200).send(results)
-    }) 
+    });
 });     
  
 
 // question 2 
-app.get('/provider', (err, results) => {
+app.get('/provider', (req, res) => {
     const provider = 'SELECT first_name, last_name, provider_specialty FROM providers'
-    db.connect(provider, (req, res) => {
+    db.query(provider, (err, results) => {
         if (err){
             return res.status(500).send('Failed to get providers information', err)      
           }
           res.status(200).send(results)
-    }) 
+    });
 });     
 
 // question 3 
-app.get('/patients2', (err, results) => {
+app.get('/patients2', (req, res) => {
     const patients2 = 'SELECT * FROM patients ORDER BY first_name;'
-    db.connect(patients2, (req, res) => {
+    db.query(patients2, (err, results) => {
         if (err){
             return res.status(500).send('Failed to get patients information', err)      
           }
           res.status(200).send(results)
-    }) 
+    });
 });     
 
 
 // question 4 
-app.get('/provider2', (err, results) => {
-    const provider2= 'SELECT * FROM providers ORDER BY provider_specialty'
-    db.connect(provider2, (req, res) => {
+app.get('/provider2', (req, res) => {
+    const provider2 = 'SELECT * FROM providers ORDER BY provider_specialty'
+    db.query(provider2, (err, results) => {
         if (err){
             return res.status(500).send('Failed to get providers information', err)      
           }
           res.status(200).send(results)
-    }) 
+    });
 });     
-app.listen(4500,() =>{
+
+// listen to the server 
+app.listen(4500, () =>{
         console.log('Server is running on port 4500.......')
 });
